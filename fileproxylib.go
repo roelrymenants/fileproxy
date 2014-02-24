6a4ec1a15c713901c62814d662587cc6cb789016
Roel Rymenants <roelrymenants@users.noreply.github.com>
2014-02-24 21:45:06 +0100

    Small changes

--- fileproxylib.go
+++ fileproxylib.go
@@ -15,6 +15,10 @@ type Config struct {
 	Rewrites map[string]string
 }
 
+func NewConfig() *Config {
+	return &Config{Rewrites: make(map[string]string)}
+}
+
 func LoadConfig(filepath string) *Config {
 	var config Config
 	b, err := ioutil.ReadFile(filepath)
@@ -25,6 +29,16 @@ func LoadConfig(filepath string) *Config {
 	log.Printf("%v", config)
 
 	return &config
+}
+
+func (config *Config) SaveToFile(filepath string) {
+	serialConfig, err := json.Marshal(config)
+
+	if err != nil {
+		log.Fatalf("Error (%s) writing config file", err)
+	}
+
+	ioutil.WriteFile(filepath, serialConfig, 0644)
 }
 
 func StartWatching(filepath string) chan Config {
